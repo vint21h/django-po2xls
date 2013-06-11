6d80087f32fc4426e47f33699e281b22aa4d6c0a
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2013-06-11 22:23:51 +0300

    fix classifiers

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "Environment :: Plugins",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: Unix",
-        "Programming Language :: Python :: 2.6"
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Utilities",
     ]
