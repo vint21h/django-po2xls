e76d26cfae6d9e5ca95ff053d05a00f875875019
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2017-04-10 16:54:40 +0300

    fix converter django support by django security policy

--- setup.py
+++ setup.py
@@ -11,16 +11,17 @@ from setuptools import (
 
 
 # metadata
-VERSION = (0, 3, 0)
+VERSION = (0, 4, 0)
 __version__ = ".".join(map(str, VERSION))
 setup(
     name="django-po2xls",
     version=__version__,
     packages=find_packages(),
     install_requires=[
-        "django-rosetta==0.7.12",
+        "Django",
+        "django-rosetta==0.7.13",
         "polib==1.0.8",
-        "xlwt==1.1.2",
+        "xlwt==1.2.0",
     ],
     author="Alexei Andrushievich",
     author_email="vint21h@vint21h.pp.ua",
@@ -35,15 +36,12 @@ setup(
         "Environment :: Plugins",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: Unix",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Utilities",
-        "Framework :: Django :: 1.5",
-        "Framework :: Django :: 1.6",
-        "Framework :: Django :: 1.7",
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.9",
         "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
     ]
 )
